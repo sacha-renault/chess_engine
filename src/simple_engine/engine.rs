4b2816ed46e5fa4b3b1d40daa03b4013ce5368ca
sacha-renault <sacha35340@gmail.com>
2024-12-18 16:43:07 +0100

    fix: fix main play function for engine, chunk in subfunction for clairity

--- src/simple_engine/engine.rs
+++ src/simple_engine/engine.rs
@@ -1,6 +1,5 @@
-use super::board::{Board, ColorBoard};
+use super::board::{Board, ColorBoard, Color};
 use super::pieces::Pieces;
-use super::static_positions;
 use super::utility::{
     coordinates_to_u64, get_color, get_piece_type, get_possible_move, is_king_checked, move_piece,
 };
@@ -9,8 +8,8 @@ pub struct Engine {
     // rules
     board: Board,
     white_turn: bool,
-    en_passant: Option<usize>, // Optional field for en passant target square
-    castling_rights: (bool, bool, bool, bool), // (white_king_side, white_queen_side, black_king_side, black_queen_side)
+    // en_passant: Option<usize>, // Optional field for en passant target square
+    // castling_rights: (bool, bool, bool, bool), // (white_king_side, white_queen_side, black_king_side, black_queen_side)
     halfmove_clock: u32, // Number of halfmoves since the last pawn move or capture
     fullmove_number: u32, // Number of full moves in the game
 }
@@ -20,69 +19,99 @@ impl Engine {
         Engine {
             board: Board::new(),
             white_turn: true,
-            en_passant: None,
-            castling_rights: (true, true, true, true),
+            // en_passant: None,
+            // castling_rights: (true, true, true, true),
             halfmove_clock: 0,
             fullmove_number: 0,
         }
     }
 
+    /// Main play function
     pub fn play(&mut self, current: (usize, usize), target: (usize, usize)) -> Result<(), String> {
-        // get user input as u64 position
         let current_square = coordinates_to_u64(current);
         let target_square = coordinates_to_u64(target);
 
-        // get boards & bitboards for this turn
-        let (board, opponent_board) = self.get_half_turn_boards();
-        let same_color_bitboard = board.bitboard();
-        let other_color_bitboard = opponent_board.bitboard();
+        // Validate the move and get the new board state
+        let new_board = self.validate_move(current_square, target_square)?;
 
-        // get the piece type and early exit if nothing found
-        let piece_type = get_piece_type(board, current_square);
-        println!("{:?}", piece_type);
+        // Apply the new board state
+        self.board = new_board;
 
-        // ensure a piece can be moved at this location
+        // Finalize the turn
+        self.finalize_turn();
+
+        Ok(())
+    }
+
+
+    /// Validate the move before overwrite board state
+    fn validate_move(&self, current_square: u64, target_square: u64) -> Result<Board, String> {
+        let (player_board, opponent_board) = self.get_half_turn_boards();
+        let piece_type = get_piece_type(player_board, current_square);
+
+        // Ensure there is a piece at the current square
         if piece_type.is_none() {
             return Err("No piece at this location".to_string());
         }
 
-        // get color for this turn
         let piece = piece_type.unwrap();
         let color = get_color(self.white_turn);
 
-        // get possible move for this piece
+        // Get the possible moves for the piece
         let possible_moves = get_possible_move(
             &piece,
             current_square,
-            same_color_bitboard,
-            other_color_bitboard,
+            player_board.bitboard(),
+            opponent_board.bitboard(),
             &color,
         );
 
-        // if target square is in the possible moves, then we can play
+        // Check if the target square is a valid move
         if target_square & possible_moves == 0 {
             return Err("This isn't a possible move".to_string());
         }
 
-        // if this is okay, we copy the board and make the move
-        let mut next_board = self.board.clone();
+        // Simulate the move and check if the king is in check
+        let simulated_board = self.simulate_and_check_move(current_square, target_square, &piece, &color)?;
 
-        // we compute the move
-        next_board = move_piece(next_board, current_square, target_square, &color, &piece);
+        Ok(simulated_board)
+    }
 
-        // Compute all the moves and ensure king isn't checked
-        if is_king_checked(board.king, opponent_board, board, &color) {
-            return Err("King is checked".to_string());
+    /// Simulate and check if the king is in check
+    fn simulate_and_check_move(
+        &self,
+        current_square: u64,
+        target_square: u64,
+        piece: &Pieces,
+        color: &Color,
+    ) -> Result<Board, String> {
+        // Simulate the move
+        let simulated_board = move_piece(self.board.clone(), current_square, target_square, color, piece);
+
+        // Get the simulated player's and opponent's boards
+        let (player_board, opponent_board) = if *color == Color::White {
+            (&simulated_board.white, &simulated_board.black)
+        } else {
+            (&simulated_board.black, &simulated_board.white)
+        };
+
+        // Check if the king is in check in the simulated state
+        // For that, we check all possible moves for next round (bulk computed all opponent moves)
+        // and check if kinng_bitbord & all_moves == 0 => no check
+        if is_king_checked(player_board.king, opponent_board, player_board, color) {
+            return Err("Move leaves the king in check".to_string());
         }
 
-        // if we arrive here is means it's all good, now we just have
-        // to replace the current board with next board and change turn
-        self.board = next_board;
-        self.halfmove_clock += 1;
-        self.white_turn = !self.white_turn;
+        Ok(simulated_board)
+    }
 
-        // We have some more extra steps after i know
-        Ok(())
+    /// Finalize the turn after a move
+    fn finalize_turn(&mut self) {
+        self.halfmove_clock += 1;
+        if !self.white_turn {
+            self.fullmove_number += 1;
+        }
+        self.white_turn = !self.white_turn;
     }
 
     fn get_half_turn_boards(&self) -> (&ColorBoard, &ColorBoard) {
@@ -101,29 +130,5 @@ impl Engine {
 
     pub fn board(&self) -> &Board {
         &self.board
-    }
-
-    pub fn pawn_moves(&self) -> u64 {
-        todo!()
-    }
-
-    pub fn rooks_moves(&self) -> u64 {
-        todo!()
-    }
-
-    pub fn strait_moves(&self) -> u64 {
-        todo!()
-    }
-
-    pub fn diagonal_moves(&self) -> u64 {
-        todo!()
-    }
-
-    pub fn queen_moves(&self) -> u64 {
-        self.strait_moves() | self.diagonal_moves()
-    }
-
-    pub fn king_moves(&self) -> u64 {
-        todo!()
     }
 }
