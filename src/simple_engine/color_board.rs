2b7f1c559d3969a0c0c7dfd41774f484552d35f2
sacha-renault <sacha35340@gmail.com>
2024-12-18 19:55:14 +0100

    docs: add documentation for ColorBoard struct and its methods

--- src/simple_engine/color_board.rs
+++ src/simple_engine/color_board.rs
@@ -1,5 +1,6 @@
 use super::pieces::{Pieces, ALL_PIECES};
 
+/// Represents a color-specific board with bitboards for each piece type.
 #[derive(Clone)]
 pub struct ColorBoard {
     pub pawn: u64,
@@ -11,10 +12,21 @@ pub struct ColorBoard {
 }
 
 impl ColorBoard {
+    /// Returns a combined bitboard of all pieces for the color.
+    ///
+    /// # Returns
+    /// A `u64` representing the combined bitboard.
     pub fn bitboard(&self) -> u64 {
         self.pawn | self.knight | self.bishop | self.rook | self.queen | self.king
     }
 
+    /// Returns the bitboard for a specific piece type.
+    ///
+    /// # Arguments
+    /// * `piece` - A reference to the `Pieces` type.
+    ///
+    /// # Returns
+    /// A `u64` representing the bitboard for the specified piece.
     pub fn get_bitboard_by_type(&self, piece: &Pieces) -> u64 {
         match piece {
             Pieces::Pawn => self.pawn,
@@ -26,6 +38,11 @@ impl ColorBoard {
         }
     }
 
+    /// Sets the bitboard for a specific piece type.
+    ///
+    /// # Arguments
+    /// * `piece` - A reference to the `Pieces` type.
+    /// * `new_bitboard` - A `u64` representing the new bitboard.
     pub fn set_bitboard_by_type(&mut self, piece: &Pieces, new_bitboard: u64) {
         match piece {
             Pieces::Pawn => self.pawn = new_bitboard,
@@ -37,6 +54,10 @@ impl ColorBoard {
         }
     }
 
+    /// Returns a vector of tuples containing the bitboard position and piece type for each piece.
+    ///
+    /// # Returns
+    /// A `Vec` of tuples where each tuple contains a `u64` bitboard position and a reference to `Pieces`.
     pub fn individual_pieces(&self) -> Vec<(u64, &Pieces)> {
         let mut result = Vec::new();
         for piece in ALL_PIECES.iter() {
