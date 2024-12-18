6f6bd2d620cc2ae4c6bb5fe1edb24d9e3d9e007c
sacha-renault <sacha35340@gmail.com>
2024-12-18 16:42:34 +0100

    feat: force enum on certain values

--- src/simple_engine/pieces.rs
+++ src/simple_engine/pieces.rs
@@ -1,9 +1,18 @@
 #[derive(Debug)]
 pub enum Pieces {
-    King,
-    Queen,
-    Rook,
-    Bishop,
-    Knight,
-    Pawn,
+    King = 6,
+    Queen = 5,
+    Rook = 4,
+    Bishop = 3,
+    Knight = 2,
+    Pawn = 1,
 }
+
+pub const ALL_PIECES: [Pieces; 6] = [
+    Pieces::Pawn,
+    Pieces::Knight,
+    Pieces::Bishop,
+    Pieces::Rook,
+    Pieces::Queen,
+    Pieces::King,
+];
