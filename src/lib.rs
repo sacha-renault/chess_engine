37ab920b8379ea9974845eb58c0f177e6f23387d
sacha renault <sacha35340@gmail.com>
2025-01-13 15:06:51 +0100

    feat: add utility functions for coordinate conversion

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,5 @@ pub mod prelude {
     pub use super::simple_engine::engine::Engine;
     pub use super::simple_engine::move_results::{CorrectMoveResults, IncorrectMoveResults};
     pub use super::simple_engine::player_move::{CastlingMove, NormalMove, PlayerMove};
+    pub use super::simple_engine::utility::{coordinates_to_u64, u64_to_coordinates};
 }
