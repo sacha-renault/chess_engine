11674f22d6dd0e1286acc5522096515caa2aa9ae
sacha renault <sacha35340@gmail.com>
2025-01-25 10:35:46 +0100

    refactor: clear lib.rs as it isn't used yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,1 @@
-pub mod boards;
-pub mod game_engine;
-pub mod pieces;
-pub mod smart_engine;
 
-// Define the prelude module
-pub mod prelude {
-    pub use super::game_engine::engine::Engine;
-    pub use super::game_engine::move_results::{CorrectMoveResults, IncorrectMoveResults};
-    pub use super::game_engine::player_move::{CastlingMove, NormalMove, PlayerMove};
-    pub use super::game_engine::utility::{
-        coordinates_to_u64, create_normal_move, iter_into_u64, u64_to_coordinates,
-    };
-}
