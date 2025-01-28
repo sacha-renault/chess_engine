3ce5ae58c1bb8360d596452a88d1e9420a237cc1
Sacha RENAULT <sacha35340@gmail.com>
2025-01-28 13:48:00 +0100

    feat: integrate game engine for move validation in PGN parsing

--- src/database/pgn_utility.rs
+++ src/database/pgn_utility.rs
@@ -1,6 +1,8 @@
 use regex::Regex;
 use sha2::{Sha256, Digest};
 
+use crate::game_engine::engine::Engine;
+
 pub fn hash_pgn(pgn: &str) -> Vec<u8> {
     let mut hasher = Sha256::new();
     hasher.update(pgn);
@@ -40,12 +42,17 @@ pub fn parse_pgn(pgn: &str, max_moves: usize) -> Result<Vec<(String, String, f32
     // init a boolean that will swap at every iteration
     let mut white_to_play = true;
 
+    // init an engine to test the validity of the moves
+    let mut engine = Engine::new();
+
     for (i, str_mv) in moves.iter().enumerate() {
         if i / 2 > max_moves {
             break;
         }
 
-        let fen = "engine".to_string();
+        let fen = engine.to_string();
+        let mv = engine.get_move_by_str(str_mv)?;
+        engine.play(mv).map_err(|_| ())?;
 
         if white_to_play {
             move_vec.push((fen, str_mv.to_string(), white_res));
